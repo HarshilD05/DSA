336f7e5c7fc2b62eb9b2dfc33629725467585f99
Harshil Damania <hdamania31@gmail.com>
2025-05-22 07:54:28 +0530

    Functionised insertion.c

--- Sorting/insertion.c
+++ Sorting/insertion.c
@@ -1,33 +1,44 @@
 #include<stdio.h>
 
-void main() {
-    int parse =0;
-    int arr[] = {51, 4, 80, 4, 89, 67, 88, 83, 13, 77};
-    int arrlen = sizeof(arr) / sizeof(arr[0]);
+void PrinArray(int arr[], size_t len) {
+    printf("\n[");
+    for (int k=0;k<arrlen;k++) {
+            printf("%d, ", arr[k]);
+        }
+    }
+    printf("]\n");
+}
 
+void insertionSort(int arr[], size_t len) {
+    int parse = 0;
     for (int i = 0;i<arrlen;i++) {
-        for (int j = i;j>0;j--) {
+        for (int j = i-1;j>=0;j--) {
             // Parse
             parse++;
             printf("\nParse %d : \n",parse-1);
-            for (int x = 0;x<arrlen;x++) {
-                printf("%d ",arr[x]);
-            }
+            PrintArray(arr, len);
+            
             // if the element before j is larger then swap
-            if (arr[j] < arr[j-1]) {
+            if (arr[j] > arr[j+1]) {
                 //swap
                 int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
             else break;
         }
     }
+}
+
+void main() {
+    int parse = 0;
+    int arr[] = {51, 4, 80, 4, 89, 67, 88, 83, 13, 77};
+    int arrlen = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, arrlen);
+    
     // Sorted Array
     printf("\nAfter Sorting....\n");
-    for (int x = 0;x<arrlen;x++) {
-        printf("%d ",arr[x]);
-    }
-
+    PrintArray(arr, arrlen);
 }
 
