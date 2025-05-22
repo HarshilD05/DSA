9e40477c447e026e4de4dd6901486baa3ef3ff94
Harshil Damania <hdamania31@gmail.com>
2025-05-22 07:58:51 +0530

    Functionised bubble.c

--- Sorting/bubble.c
+++ Sorting/bubble.c
@@ -1,29 +1,41 @@
 // Bubble Sort
 #include<stdio.h>
-void main() {register int i,j,parse = 1 ;
-    int arr[] = {19 ,13 ,43 ,65 ,60 ,23 ,28 ,40 ,56 ,58};
-    int arrlen = sizeof(arr) / sizeof(arr[0]);
-    // Sorting
-    for (j=0;j<arrlen-1;j++) {
-        for (i=0;i<arrlen-j-1;i++) {
+
+void PrinArray(int arr[], size_t len) {
+    printf("\n[");
+    for (int k=0;k<len;k++) {
+            printf("%d, ", arr[k]);
+        }
+    }
+    printf("]\n");
+}
+
+void bubbleSort(int arr[], size_t len) {
+    register int i,j,parse = 1;
+    for (i=0;i<len-1;i++) {
+        for (j=0;j<len-i-1;j++) {
             // if next value is smaller than current value swap
-            if (arr[i+1] < arr[i]) {
+            if (arr[j+1] < arr[j]) {
                 //Swap
-                int temp = arr[i+1];
-                arr[i+1] = arr[i];
-                arr[i] = temp;
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
             }
             //Parse
             printf("\n Parse %d: \n",parse);
-            for (int k=0;k<arrlen;k++) {
-                printf("%d ",arr[k]);
-            }
+            PrintArray(arr, len);
             parse++;
         }
     }
+}
+
+void main() {
+    int arr[] = {19 ,13 ,43 ,65 ,60 ,23 ,28 ,40 ,56 ,58};
+    int arrlen = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, arrlen);
+    
     //Output
     printf("\n Sorted Array : \n");
-    for (i=0;i<arrlen;i++) {
-        printf("%d ",arr[i]);
-    }
+    PrintArray(arr, arrlen);
 }
